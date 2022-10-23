d53be8a48a372c742aacaba096a3a730cf6440b1
camjam21167 <csiebert8910@gmail.com>
2022-10-23 16:48:38 -0500

    Finished TruckManager HW

--- CSC3660/InClass7/src/TruckHW/TruckManager.java
+++ CSC3660/InClass7/src/TruckHW/TruckManager.java
@@ -10,32 +10,64 @@ public class TruckManager {
         HashMap<Integer, String> HMData = new HashMap<>();
         ArrayList<String> data = getFileData(inFile);
         Deque<String> data2 = getDeque(data);
-        showTrucks(data2, HMData);
-        int uIn = getUserInput(HMData);
+        boolean go = true;
+        while (go){
+            showTrucks(data2, HMData);
+            String uIn = getUserInput(HMData);
+            removeTruck(uIn, data2, HMData);
+        }
     }
 
-    private static int getUserInput(HashMap<Integer, String> tIDs) {
+    public static void removeTruck(String uIn, Deque<String> data2, HashMap<Integer, String> HMData) {
+        System.out.printf("\nYou must remove these truck(s) first: ");
+        System.out.printf("\n-----Showing truck(s) you must remove first-----");
+        for(String row : data2){
+            String[] toks = row.split(",");
+            int truckID = Integer.parseInt(toks[0]);
+            int zip = Integer.parseInt(toks[1]);
+            int packages = Integer.parseInt(toks[2]);
+            if(truckID == Integer.parseInt(uIn)){
+                data2.remove(row);
+                removeFromHM(HMData, uIn);
+                break;
+            }
+            System.out.printf("\nTruckID: %s Zip: %s Number of Packages: %s", truckID, zip, packages);
+        }
+    }
+
+    private static void removeFromHM(HashMap<Integer, String> hmData, String uIn) {
+        TreeSet<Integer> keys = new TreeSet<>(hmData.keySet());
+        for (Integer key : keys){
+            if(Integer.parseInt(uIn) == key){
+                hmData.remove(key);
+            }
+        }
+    }
+
+    public static String getUserInput(HashMap<Integer, String> tIDs) {
         Scanner scan = new Scanner(System.in);
-        Set<Integer> keys = tIDs.keySet();
+        TreeSet<Integer> keys = new TreeSet<>(tIDs.keySet());
         System.out.printf("\nEnter truck to remove[(%s)]: ", keys);
-        int uIn = 0;
+        String uIn = "";
         try{
-            uIn = scan.nextInt();
-            if(keys.contains(uIn)){
+            uIn = scan.nextLine();
+            if(keys.contains(Integer.parseInt(uIn))){
                 return uIn;
             } else {
                 System.out.printf("\nNot a valid input, please enter a number in range %s: ", keys);
-                uIn = scan.nextInt();
+                uIn = scan.nextLine();
             }
         }catch (Exception e){
+            if (uIn.equalsIgnoreCase("Q")){
+                System.exit(1);
+            }
             System.out.printf("\nPlease enter an integer value.");
-            System.out.printf("\nNot a valid input, please enter a number in range %s: ", keys);
-            uIn = scan.nextInt();
+            uIn = getUserInput(tIDs);
         }
         return uIn;
     }
 
-    private static Deque<String> getDeque(ArrayList<String> data) {
+    public static Deque<String> getDeque(ArrayList<String> data) {
         Deque<String> reformdata = new ArrayDeque<>();
         for(int i = 1; i < data.size(); i++){
             reformdata.push(data.get(i));
@@ -43,7 +75,7 @@ public class TruckManager {
         return reformdata;
     }
 
-    private static void showTrucks(Deque<String> data, HashMap<Integer, String> tIDs) {
+    public static void showTrucks(Deque<String> data, HashMap<Integer, String> tIDs) {
         System.out.printf("\n------Showing the current alley of trucks------");
         for(String row : data){
             String[] toks = row.split(",");
@@ -56,7 +88,7 @@ public class TruckManager {
         }
     }
 
-    private static ArrayList<String> getFileData(String inFile) {
+    public static ArrayList<String> getFileData(String inFile) {
         FileIOV2 fio = new FileIOV2(inFile);
         try {
             fio.setFileData();
