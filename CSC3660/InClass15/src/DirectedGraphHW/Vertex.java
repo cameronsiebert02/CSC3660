c0329d00c59735d60f088d812af07f89474516ee
camjam21167 <csiebert8910@gmail.com>
2022-12-07 08:33:08 -0600

    DGHW - Imported files to IDE

---
+++ CSC3660/InClass15/src/DirectedGraphHW/Vertex.java
@@ -0,0 +1,10 @@
+package DirectedGraphHW;
+
+public class Vertex {
+    String label;
+    int weight;
+    public Vertex(String label, int weight) {
+        this.label = label;
+        this.weight = weight;
+    }
+}
